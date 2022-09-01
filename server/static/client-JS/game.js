d774e4f138331b63151c50b95d88fa6951ca7ec0
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2022-09-02 05:26:38 +0530

    Format & remove keypress event from game once over

--- server/static/client-JS/game.js
+++ server/static/client-JS/game.js
@@ -1,201 +1,194 @@
 // JS for the HTML
 
 const eventHandlers = {
-    settingsToggle: function () {
-        let settings = document.getElementById('gameSettings');
-        settings.classList.toggle('hidden');
-    }
-}
+  settingsToggle: function () {
+    let settings = document.getElementById("gameSettings");
+    settings.classList.toggle("hidden");
+  },
+};
 
 function addEventHandlers() {
+  let settings_gear = document.getElementById("settingsGear");
 
-    let settings_gear = document.getElementById('settingsGear');
-
-    settings_gear.addEventListener('click', eventHandlers.settingsToggle);
+  settings_gear.addEventListener("click", eventHandlers.settingsToggle);
 }
-
 
 // Socket IO stuff
 
 const socket = io();
 
 function changeRoom() {
-    let btn = document.getElementById("changeRoomBtn");
-    let room_entry = document.getElementById("roomId");
-    let curRoom = document.getElementById("roomId-str");
+  let btn = document.getElementById("changeRoomBtn");
+  let room_entry = document.getElementById("roomId");
+  let curRoom = document.getElementById("roomId-str");
 
-    curRoom = curRoom.textContent.slice(17);
-    let room = room_entry.value;
+  curRoom = curRoom.textContent.slice(17);
+  let room = room_entry.value;
 
-    if (room && room !== curRoom) {
-        socket.emit('room-change', Number(room));
-        room_entry.value = '';
-    }
+  if (room && room !== curRoom) {
+    socket.emit("room-change", Number(room));
+    room_entry.value = "";
+  }
 }
 
 function changeUsername() {
-    let userString = document.getElementById("username-str");
-    let username = userString.dataset.str;
+  let userString = document.getElementById("username-str");
+  let username = userString.dataset.str;
 
-    let usernameInp = document.getElementById("usernameInp");
-    let inpString = usernameInp.value;
+  let usernameInp = document.getElementById("usernameInp");
+  let inpString = usernameInp.value;
 
-    if (inpString && username !== inpString) {
-        socket.emit("username-change", inpString)
-        usernameInp.value = '';
-    }
-
+  if (inpString && username !== inpString) {
+    socket.emit("username-change", inpString);
+    usernameInp.value = "";
+  }
 }
 
-
-socket.on('connect', () => {
-    console.log(socket.id);
+socket.on("connect", () => {
+  console.log(socket.id);
 });
 
-socket.on('error', (msg) => {
-    alert(msg);
-})
+socket.on("error", (msg) => {
+  alert(msg);
+});
 
-socket.on('client-settings', (json) => {
-    let username = json.username;
-    let room = json.room;
+socket.on("client-settings", (json) => {
+  let username = json.username;
+  let room = json.room;
 
-    let curUserEl = document.getElementById('username-str');
-    let curUser = curUserEl.dataset.str;
+  let curUserEl = document.getElementById("username-str");
+  let curUser = curUserEl.dataset.str;
 
-    if (!curUser || curUser !== username) {
-        curUserEl.dataset.str = username;
-        curUserEl.textContent = `Username: ${username}`
+  if (!curUser || curUser !== username) {
+    curUserEl.dataset.str = username;
+    curUserEl.textContent = `Username: ${username}`;
+  }
+
+  let curRoomEl = document.getElementById("roomId-str");
+  let curRoom = curRoomEl.dataset.room;
+
+  if (!curRoom || curRoom !== room) {
+    curRoomEl.dataset.room = room;
+    curRoomEl.textContent = `Current room ID: ${room}`;
+  }
+
+  console.log("Updated settings");
+});
+
+socket.on("lobby-update", (rooms) => {
+
+  let table = document.getElementById("lobbyTable");
+
+  let rowList = [];
+
+  for (let room in rooms) {
+    let row = document.createElement("tr");
+
+    let td_num = document.createElement("td");
+    td_num.classList.add("lobby-row", "row");
+    td_num.textContent = room;
+
+    let td_state = document.createElement("td");
+    let circle = document.createElement("div");
+
+    circle.classList.add("circle");
+
+    if (rooms[room] <= 3) {
+      circle.classList.add("open-lobby");
+    } else {
+      circle.classList.add("closed-lobby");
     }
 
-    let curRoomEl = document.getElementById('roomId-str');
-    let curRoom = curRoomEl.dataset.room;
+    td_state.append(circle);
 
-    if (!curRoom || curRoom !== room) {
-        curRoomEl.dataset.room = room;
-        curRoomEl.textContent = `Current room ID: ${room}`;
-    }
+    row.append(td_num, td_state);
 
-    console.log('Updated settings')
-})
+    rowList.push(row);
+  }
 
-socket.on('lobby-update', (rooms) => {
-    console.log(rooms)
-
-    let table = document.getElementById('lobbyTable');
-
-    let rowList = []
-
-    for (let room in rooms) {
-        let row = document.createElement('tr');
-        
-        let td_num = document.createElement('td');
-        td_num.classList.add('lobby-row', 'row');
-        td_num.textContent = room;
-
-        let td_state = document.createElement('td');
-        let circle = document.createElement('div');
-
-        circle.classList.add('circle');
-
-        if (rooms[room] <= 3){
-            circle.classList.add('open-lobby');
-        } else {
-            circle.classList.add('closed-lobby');
-        }
-
-        td_state.append(circle);
-
-        row.append(td_num, td_state);
-
-        rowList.push(row);        
-    }
-
-    table.replaceChildren(...rowList);
-
-})
-
+  table.replaceChildren(...rowList);
+});
 
 // Game events
 
-socket.on('start-game', () => {
-    console.log('Starting game');
-    game.initGame()
+socket.on("start-game", () => {
+  console.log("Starting game");
+  game.initGame();
 });
 
-socket.on('waiting-game', () => {
-    console.log('Waiting for other players')
-})
+socket.on("waiting-game", () => {
+  console.log("Waiting for other players");
+});
 
-socket.on('game-score', (score) => {
-    console.log(score)
-})
-
+socket.on("game-score", (score) => {
+  console.log(score);
+});
 
 const game = {
+  maxTime: 10,
+  totalPress: 0,
+  score: 0,
 
-    maxTime: 10,
-    totalPress: 0,
-    score: 0,
+  start_time: undefined,
+  current_time: undefined,
 
+  mash_key: " ",
 
-    start_time: undefined,
-    current_time: undefined,
+  // FPS
+  freq: 30,
+  tick: 0,
 
-    mash_key: ' ',
+  resetGame: function () {
+    game.totalPress = 0;
+    game.score = 0;
 
-    // FPS
-    freq: 30,
-    tick: 0,
+    game.start_time = undefined;
+    game.current_time = undefined;
 
-    resetGame: function () {
-        game.totalPress = 0;
-        game.score = 0
+    game.tick = 0;
+  },
 
-        game.start_time = undefined;
-        game.current_time = undefined;
+  initGame: function () {
+    game.setCallbacks();
 
-        game.tick = 0
-    },
+    game.start_time = new Date().getTime();
 
-    initGame: function () {
-        game.setCallbacks();
+    let gameloop = setInterval(game.gameLoop, 1000 / game.freq);
+    setTimeout(() => game.gameEnd(gameloop), 1000 * game.maxTime);
+  },
 
-        game.start_time = new Date().getTime();
+  setCallbacks: function () {
+    let gameContainer = document.getElementById("gameMain");
+    gameContainer.addEventListener("keydown", game.__keypress__);
+    gameContainer.focus();
+  },
 
-        let gameloop = setInterval(game.gameLoop, 1000 / game.freq);
-        setTimeout(() => game.gameEnd(gameloop), 1000 * game.maxTime);
-    },
+  gameLoop: function () {
+    game.current_time = new Date().getTime();
+    game.score =
+      game.totalPress / ((game.current_time - game.start_time) / 1000);
+    game.tick += 1;
 
-    setCallbacks: function () {
-        let gameContainer = document.getElementById('gameMain');
-        gameContainer.addEventListener('keydown', (e) => {
-            if (game.mash_key === e.key) {
-                game.totalPress += 1;
-            }
-        });
-        gameContainer.focus();
-    },
+    let msg = {
+      tick_id: game.tick,
+      score: game.score,
+    };
 
-    gameLoop: function () {
-        game.current_time = new Date().getTime();
-        game.score = game.totalPress / ((game.current_time - game.start_time) / 1000);
-        game.tick += 1;
+    socket.emit("game-tick", msg);
+  },
 
-        let msg = {
-            'tick_id': game.tick,
-            'score': game.score
-        };
+  gameEnd: function (loop) {
+    clearInterval(loop);
+    socket.emit("game-end");
+    game.resetGame();
 
-        socket.emit('game-tick', msg);
-    },
+    let gameContainer = document.getElementById("gameMain");
+    gameContainer.removeEventListener("keydown", game.__keypress__);
+  },
 
-    gameEnd: function (loop) {
-        clearInterval(loop);
-        socket.emit('game-end')
-        game.resetGame()
+  __keypress__: function (e) {
+    if (game.mash_key == e.key) {
+      game.totalPress += 1;
     }
-
-
-
-}
+  },
+};
