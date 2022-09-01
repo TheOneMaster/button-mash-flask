bae06a152ff06d1485636a4f911c9f78d530d4d9
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2022-09-01 05:50:14 +0530

    Base game server and client side functionality
    - The outline for the HTML Page and the CSS is done
    - Server side code to store and send game data to clients is done
    - Client Side JS still requires a method of visualising the game score
    - Scores from each client in room per tick logged in console

--- server/game.py
+++ server/game.py
@@ -1,32 +1,43 @@
-from flask import request, current_app
-from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask import request, session
+from flask_socketio import SocketIO, join_room, leave_room, emit, rooms
+from flask_login import current_user
 
 from random import randint
+import json
+from uuid import uuid4
+from random_username.generate import generate_username
+from functools import wraps
 
-socket = SocketIO(ping_interval=.5, ping_timeout=1)
+from .database import Game
+
+socket = SocketIO()
 
 DEFAULT_ROOM = 1000
-ROOM_CLIENT_MAP = {1000: set()}
 
-def set_room(room=DEFAULT_ROOM):
+ROOM_CLIENT_MAP = {}   # Map clients to a room, allows us to query list of clients connected to specific room
+CLIENT_USERNAME = {}   # Map client socket ID to username
+ROOM_SCORE = {}        # Map score per tick to room
+
+def set_room(room):
     global ROOM_CLIENT_MAP
     
     if room not in ROOM_CLIENT_MAP:
-        ROOM_CLIENT_MAP[room] = set([request.sid])
+        ROOM_CLIENT_MAP[room] = {request.sid: 0}
     
     elif len(ROOM_CLIENT_MAP[room]) <= 3:
-        ROOM_CLIENT_MAP[room].add(request.sid)
+        ROOM_CLIENT_MAP[room][request.sid] = 0
     
     else:
         while len(ROOM_CLIENT_MAP[room]) >= 4:
             room = randint(0, 1000)
             
             if room not in ROOM_CLIENT_MAP:
-                ROOM_CLIENT_MAP[room] = set()
+                ROOM_CLIENT_MAP[room] = {}
         
-        ROOM_CLIENT_MAP[room].add(request.sid)
+        ROOM_CLIENT_MAP[room][request.sid] = 0
         
     join_room(room)
+    session['room'] = room
     
     return room
 
@@ -34,30 +45,200 @@ def remove_room():
     global ROOM_CLIENT_MAP
     
     sock_id = request.sid
+    r_room = None
+    empty_room = False
+    
     for room, conn in ROOM_CLIENT_MAP.items():
         if sock_id in conn:
-            conn.remove(sock_id)
+            conn.pop(sock_id)
+            
+            if len(conn) == 0:
+                empty_room = True
+            
+            r_room = room
+            leave_room(r_room)
+    
+    if empty_room:
+        ROOM_CLIENT_MAP.pop(r_room)
+    
+    return r_room
+    
+        
+    
 
+def getRoom():
+    
+    room = None
+    for i in rooms():
+        if i in ROOM_CLIENT_MAP:
+            room = i
+    
+    return room
+
+"""
+Helper functions for the socketIO connection. Use to automatically emit events when changes are made to rooms/clients
+"""
+
+def update_lobby(fn):
+    """
+    Decorator function for functions that cause changes to the lobby. Sends 
+    """
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        
+        out = fn(*args, **kwargs)
+        
+        room = session.get('room')
+        username = session.get('username')
+        
+        if room and username:
+    
+            msg_client = {
+                'username': username,
+                'room': room
+            }
+            
+            emit('client-settings', msg_client)
+            
+            
+            lobby = ROOM_CLIENT_MAP[room]
+            lobbyClients = {client: CLIENT_USERNAME[client] for client in lobby.keys()}
+            msg_lobby = {
+                'roomList': lobbyClients
+            }
+            
+            emit('lobby-update', msg_lobby, to=room)
+        
+        return out
+        
+    return wrapper
+
+###############
+# Base Events #
+###############
 @socket.on('connect')
-def connect():
+@update_lobby
+def setupClient():
+    """
+    First time setup when a client connects to the socket server. Sets default values for client unless logged in.
+    """    
     
-    room = set_room()
+    # Add username to client
+    username = None
+    if current_user.is_authenticated:
+        username = current_user.username
+    else:
+        username = generate_username()[0]
     
-    json = {
-        "room": room
-    }
+    usernameChange(username)
     
-    print(ROOM_CLIENT_MAP)
+    # Add client to room map
+    roomChange(DEFAULT_ROOM)
     
-    emit('settings', json)
-
+    print(f"Connected: {session['username']} from {request.remote_addr}")
 
 @socket.on('disconnect')
 def disconnect():
-    leave_room()
-    remove_room()
-    print(ROOM_CLIENT_MAP)
+    global CLIENT_USERNAME
     
-@socket.on('settings')
-def test(data):
-    print(data)
+    remove_room()
+    CLIENT_USERNAME.pop(request.sid)
+    
+    username = session['username']
+    
+    print(f'Disconnected: {username} from {request.remote_addr}')
+
+
+############
+# Settings #
+############
+
+@socket.on('username-change')
+@update_lobby
+def usernameChange(name):
+    global CLIENT_USERNAME       
+    
+    CLIENT_USERNAME[request.sid] = name
+    session['username'] = name
+
+@socket.on("room-change")
+@update_lobby
+def roomChange(room):
+    
+    remove_room()
+    set_room(room)
+
+########
+# Game #
+########
+
+@socket.on('game-ready')
+def gameReady():
+    global ROOM_SCORE
+    
+    username = session.get('userName', None)
+    
+    if username is None:
+        emit('error', "Username is not set")
+        return
+    
+    
+    roomNum = session['room'] 
+    room = ROOM_CLIENT_MAP[roomNum]
+    
+    room[request.sid] = 1
+    
+    room_isReady = all(v for v in room.values())
+    if room_isReady:
+        
+        ROOM_SCORE[roomNum] = {}
+        
+        emit('start-game', to=roomNum)
+    else:
+        emit('waiting-game')
+    
+    print(room)
+    
+
+
+@socket.on('game-tick')
+def gameTick(json):
+    global ROOM_SCORE
+    
+    room = session['room']
+    tick = json['tick_id']
+    score = json['score']
+    client = request.sid
+    
+    client_list = ROOM_CLIENT_MAP[room].keys()
+    curRoom = ROOM_SCORE[room]
+    
+    if tick not in curRoom:
+        curRoom[tick] = [None for i in client_list]
+    
+    client_index = list(client_list)
+    client_index = client_index.index(client)
+
+    lobby_score = curRoom[tick]
+    lobby_score[client_index] = round(score, 3)
+    
+    tickCompleted = None not in lobby_score
+    
+    if tickCompleted:
+        emit('game-score', lobby_score,  to=room)
+
+
+@socket.on('game-end')
+def gameEnd():
+    
+    gameId = str(uuid4())
+    room = session['room']
+    
+    ROOM_CLIENT_MAP[room][request.sid] = 2
+    
+    curRoom = ROOM_CLIENT_MAP[room]
+    if all(ready == 2 for ready in curRoom.values()):
+    
+        with open(f"{gameId}.json", 'w') as json_file:
+            score = ROOM_SCORE.pop()
+            json.dump(score, json_file)
--- server/static/client-JS/game.js
+++ server/static/client-JS/game.js
@@ -1,14 +1,162 @@
+// JS for the HTML
+
+const eventHandlers = {
+    settingsToggle: function () {
+        let settings = document.getElementById('gameSettings');
+        settings.classList.toggle('hidden');
+    }
+}
+
+function addEventHandlers() {
+
+    let settings_gear = document.getElementById('settingsGear');
+
+    settings_gear.addEventListener('click', eventHandlers.settingsToggle);
+}
+
+
+// Socket IO stuff
+
 const socket = io();
 
+function changeRoom() {
+    let btn = document.getElementById("changeRoomBtn");
+    let room_entry = document.getElementById("roomId");
+    let curRoom = document.getElementById("roomId-str");
 
-socket.on('connect', ()=>{
+    curRoom = curRoom.textContent.slice(17);
+    let room = room_entry.value;
+
+    if (room && room !== curRoom) {
+        socket.emit('room-change', Number(room));
+        room_entry.value = '';
+    }
+}
+
+function changeUsername() {
+    let userString = document.getElementById("username-str");
+    let username = userString.dataset.str;
+
+    let usernameInp = document.getElementById("usernameInp");
+    let inpString = usernameInp.value;
+
+    if (inpString && username !== inpString) {
+        socket.emit("username-change", inpString)
+        usernameInp.value = '';
+    }
+
+}
+
+
+socket.on('connect', () => {
     console.log(socket.id);
-    console.log('test')
 });
 
-socket.on('settings', (options)=>{
-    let room = options['room'];
-    let room_str = document.getElementById("roomId-str");
-
-    room_str.textContent = `Room ID: ${room}`;
+socket.on('error', (msg) => {
+    alert(msg);
 })
+
+socket.on('client-settings', (json) => {
+    let username = json.username;
+    let room = json.room;
+
+    let curUserEl = document.getElementById('username-str');
+    let curUser = curUserEl.dataset.str;
+
+    if (!curUser || curUser !== username) {
+        curUserEl.dataset.str = username;
+        curUserEl.textContent = `Username: ${username}`
+    }
+
+    let curRoomEl = document.getElementById('roomId-str');
+    let curRoom = curRoomEl.dataset.room;
+
+    if (!curRoom || curRoom !== room) {
+        curRoomEl.dataset.room = room;
+        curRoomEl.textContent = `Current room ID: ${room}`;
+    }
+
+    console.log('Updated settings')
+})
+
+socket.on('start-game', () => {
+    console.log('Starting game');
+    game.initGame()
+});
+
+socket.on('waiting-game', () => {
+    console.log('Waiting for other players')
+})
+
+socket.on('game-score', (score) => {
+    console.log(score)
+})
+
+
+const game = {
+
+    maxTime: 30,
+    totalPress: 0,
+    score: 0,
+
+
+    start_time: undefined,
+    end_time: undefined,
+
+    mash_key: ' ',
+
+    // FPS
+    freq: 30,
+    tick: 0,
+
+    resetGame: function () {
+        game.totalPress = 0;
+        game.score = 0
+
+        game.start_time = undefined;
+        game.end_time = undefined;
+
+        game.tick = 0
+    },
+
+    initGame: function () {
+        game.setCallbacks();
+
+        game.start_time = new Date().getTime();
+
+        let gameloop = setInterval(game.gameLoop, 1000 / game.freq);
+        setTimeout(() => game.gameEnd(gameloop), 1000 * game.maxTime);
+    },
+
+    setCallbacks: function () {
+        let gameContainer = document.getElementById('gameMain');
+        gameContainer.addEventListener('keydown', (e) => {
+            if (game.mash_key === e.key) {
+                game.totalPress += 1;
+            }
+        });
+        gameContainer.focus();
+    },
+
+    gameLoop: function () {
+        game.current_time = new Date().getTime();
+        game.score = game.totalPress / ((game.current_time - game.start_time) / 1000);
+        game.tick += 1;
+
+        let msg = {
+            'tick_id': game.tick,
+            'score': game.score
+        };
+
+        socket.emit('game-tick', msg);
+    },
+
+    gameEnd: function (loop) {
+        clearInterval(loop);
+        socket.emit('game-end', game.score)
+        game.resetGame()
+    }
+
+
+
+}
