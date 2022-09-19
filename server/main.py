86c2ca1149f7b424db3b6bfe2f66749ebe0d922d
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2022-09-19 16:16:30 +0530

    Redirect to game page instead of index

--- server/main.py
+++ server/main.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template, send_from_directory
+from flask import Blueprint, render_template, send_from_directory, redirect, url_for
 
 main = Blueprint("main", __name__, static_folder='static')
 
 @main.route("/")
 def home():
-    return render_template('index.jinja')
+    return redirect(url_for('main.game'))
 
 @main.route("/game")
 def game():
